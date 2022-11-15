a03a1a5052e4f3363a94a21e0234321d1664be65
truthdawn <truthdawn@naver.com>
2022-11-15 15:45:16 +0900

    금리와, VIX지수와 주가의 상관관계가 반대일때 영향 분석

--- Data/lib/mf.py
+++ Data/lib/mf.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from lib import set_up
+import pandas as pd
 
 
 def effect_day_of_week_rise(df, day, change, direction):
@@ -81,3 +83,71 @@ def plot_effect_of_day_before(df, start, end, interval):
     plt.ylabel('probability')
     plt.xlabel('% change')
     plt.show()
+
+def effect_of_vix_up (df_stock, vix_change, start_date):
+    days_down = 0
+    days_up = 0
+    df_vix = set_up.set_stock_df('vix', '^VIX', start_date)
+    for idx in df_stock.index:
+        if df_vix.at[idx, 'percentage_change'] > vix_change and df_stock.at[idx, 'percentage_change'] < 0:
+            days_down = days_down +1
+        elif df_vix.at[idx, 'percentage_change'] > vix_change and df_stock.at[idx, 'percentage_change'] > 0:
+            days_up = days_up + 1
+    probability = days_up/(days_up + days_down)*100
+    return probability
+
+def effect_of_vix_down (df_stock, vix_change, start_date):
+    days_down = 0
+    days_up = 0
+    df_vix = set_up.set_stock_df('vix', '^VIX', start_date)
+    for idx in df_stock.index:
+        if df_vix.at[idx, 'percentage_change'] < vix_change and df_stock.at[idx, 'percentage_change'] < 0:
+            days_down = days_down +1
+        elif df_vix.at[idx, 'percentage_change'] < vix_change and df_stock.at[idx, 'percentage_change'] > 0:
+            days_up = days_up + 1
+    probability = days_up/(days_up + days_down)*100
+    return probability
+
+def effect_of_vix_next_day(df_stock, start_date):
+    days_down = 0
+    days_up = 0
+    df_vix = set_up.set_stock_df('vix', '^VIX', start_date)
+    for idx in df_stock.index:
+        if df_vix.at[idx, 'percentage_change'] < 0 and df_stock.at[idx, 'percentage_change'] < 0:
+            if df_stock.shift(-1).at[idx, 'percentage_change']>0:
+                days_up = days_up + 1
+            else:
+                days_down = days_down + 1
+    probability = days_up/(days_up + days_down)*100
+    return probability
+
+
+
+            
+def merge(df1, df2, factor):
+    df1_adding = df1[factor]
+    df2_adding = df2[factor]
+    df_merged = pd.concat([df1_adding, df2_adding], axis=1)
+    df_merged.columns = ['{}_stock'.format(factor)], ['{}_control'.format(factor)]
+    return df_merged
+
+def effect_of_10Y_next_day(df_stock, factor, start_date):
+    #10년물 금리와 주가를 비교하기 전에, 휴장일을 반영하기 위해 날짜에 맞추어 데이터를 정리해준다.
+    df_10y = set_up.set_stock_df('10Y', '^TNX', start_date)
+    df_stock_adding = df_stock[factor]
+    df_10y_adding = df_10y[factor]
+    df_arranged = pd.concat([df_stock_adding, df_10y_adding], axis=1)
+    df_arranged.columns = ['{}_stock'.format(factor), '{}_control'.format(factor)]
+
+    days_down = 0
+    days_up = 0
+    #금리와 주가가 모두 상승한 조건에서 다음날 오른 경우와 하락한 경우 파악
+    for idx in df_arranged.index:
+        if df_arranged.at[idx, '{}_stock'.format(factor)] > 0 and df_arranged.at[idx, '{}_control'.format(factor)] >0:
+            if df_arranged.shift(-1).at[idx, '{}_control'.format(factor)] > 0:
+                days_up = days_up + 1
+            else:
+                days_down = days_down + 1
+                
+    probability = days_up/(days_up + days_down)*100
+    return probability
--- Data/main.py
+++ Data/main.py
@@ -10,17 +10,15 @@ plt.style.use('seaborn-v0_8')
 
 
 df = set_up.set_stock_df('nasdaq', '^ixic', '2010-01-01') # format ('name', 'ticker', 2020-01-01)
+df_Y = set_up.set_stock_df('10Y', '^TNX', '2010-01-01')
+
 #day_of_week_analysis.effect_day_of_week_rise(df, 'Friday', 3, 'down') # format (df, 'day of week', 'int', 'up or down')
 #day_of_week_analysis.day_of_week_rise(df, 'Friday')
-# mf.up_leads_up(df, 4)
-# mf.down_leads_down(df, -4)
-# data = []
-# for i in list(np.arange(-10, 0, 0.1)):
-#     data.append(mf.down_leads_down(df, i))
+#mf.up_leads_up(df, 4)
+#mf.down_leads_down(df, -4)
+#mf.plot_effect_of_day_before(df, -8, 8, 0.1)
+#print(mf.effect_of_vix_down(df, -3, '2010-01-01'))
+#print(mf.effect_of_vix_next_day(df, '2010-01-01'))
+#print(mf.effect_of_10Y_next_day(df, '2010-01-01'))
 
-# plt.figure()
-# plt.plot(list(np.arange(-10, 0, 0.1)), data)
-# plt.ylabel('probability')
-# plt.xlabel('% Drop')
-# plt.show()
-mf.plot_effect_of_day_before(df, -8, 8, 0.1)
+print(mf.effect_of_10Y_next_day(df, 'percentage_change', '2010-01-01'))
